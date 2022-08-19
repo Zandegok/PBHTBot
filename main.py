17e2e8862c021165f22be95a16960d68c364392a
Innokentii <jacksafj@gmail.com>
2022-08-19 19:51:01 +0300

    ю

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import telebot
 
+print('Я родился')
 bot = telebot.TeleBot('5679403254:AAEijeWHM9ub9MxaTRuhFrL5iIFj4Tjlp_I')
+
 
 @bot.message_handler(content_types=["text"])
 def handle_text(message):
@@ -8,4 +10,6 @@ def handle_text(message):
     match command:
         case '/echo', *t:
             bot.send_message(message.chat.id, ' '.join(*t))
+
+
 bot.polling(none_stop=True, interval=0)
