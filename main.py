017a038061a525435ecacec2b713e4a8945d0eb8
Innokentii <jacksafj@gmail.com>
2022-08-19 19:25:30 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import telebot
+
+bot = telebot.TeleBot('5679403254:AAEijeWHM9ub9MxaTRuhFrL5iIFj4Tjlp_I')
+
+@bot.message_handler(content_types=["text"])
+def handle_text(message):
+    command = message.text.split()
+    match command:
+        case '/echo', *t:
+            bot.send_message(message.chat.id, ' '.join(*t))
+bot.polling(none_stop=True, interval=0)
