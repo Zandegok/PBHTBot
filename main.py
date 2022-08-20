4c03182efcad9d2f6c098d9ddc0b5549b63b0798
Innokentii <jacksafj@gmail.com>
2022-08-20 12:05:47 +0300

    time manage

--- main.py
+++ main.py
@@ -1,14 +1,45 @@
 import telebot
+import threading
+import datetime
+import time
 
 print('Я родился')
 bot = telebot.TeleBot('5679403254:AAEijeWHM9ub9MxaTRuhFrL5iIFj4Tjlp_I')
+timeManagers = eval(open('docs/time managers').read())
+
+
+def checkTime():
+    b = False
+    while True:
+        if datetime.datetime.now().second.real == 0 and not b:
+            for chatid in timeManagers:
+                bot.send_message(chatid, 'Начало минуты:' + str(datetime.datetime.now()))
+            b = True
+        if datetime.datetime.now().second.real == 30 and b:
+            for chatid in timeManagers:
+                bot.send_message(chatid, 'Середина минуты:' + str(datetime.datetime.now()))
+            b = False
+
+
+threading.Thread(target=checkTime, args=(), name='Timer').start()
+
 
 @bot.message_handler(content_types=["text"])
 def handle_text(message):
     command = message.text.split()
+    print(message.chat)
     match command:
+        # case '/start'|'/help':
         case '/echo', *t:
             bot.send_message(message.chat.id, ' '.join(t))
+        case '/timemanage', 'on':
+            timeManagers.append(message.chat.id)
+            open('docs/time managers', 'w').write(str(timeManagers))
+            bot.send_message(message.chat.id,'Менеджмент времени подключён')
+        case '/timemanage', 'off':
+            timeManagers.remove(message.chat.id)
+            open('docs/time managers', 'w').write(str(timeManagers))
+            bot.send_message(message.chat.id, 'Менеджмент времени отключён')
 
 
 bot.polling(none_stop=True, interval=0)
